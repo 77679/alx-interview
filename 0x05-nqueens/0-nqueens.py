48939d2e6e03a27dc5791fe42a6d94b17733d1dd
77679 <106762511+77679@users.noreply.github.com>
2023-05-25 21:28:44 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """ N queens """
 import sys
-
-
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
     exit(1)
-
 if not sys.argv[1].isdigit():
     print("N must be a number")
     exit(1)
-
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
-
 n = int(sys.argv[1])
-
-
 def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
     if i < n:
@@ -26,8 +19,6 @@ def queens(n, i=0, a=[], b=[], c=[]):
                 yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
-
-
 def solve(n):
     """ solve """
     k = []
@@ -39,6 +30,4 @@ def solve(n):
         print(k)
         k = []
         i = 0
-
-
 solve(n)
